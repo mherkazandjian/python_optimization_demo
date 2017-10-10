83e8d33dbf3d373130e41f449325b28bf4be3c50
Mher Kazandjian <mherkazandjian@gmail.com>
2017-10-10 16:29:04 +0200

    delete unecessary setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-
-import numpy
-from distutils.core import setup
-from Cython.Build import cythonize
-
-setup(
-    ext_modules = cythonize("laplacian_filter_cython.pyx"),
-    include_dirs=[numpy.get_include()]
-)
